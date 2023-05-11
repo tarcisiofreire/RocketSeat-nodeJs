4ded69a04cf654c51e306a24d302c07bfb81606f
tarcisiofreire <tarcisiorfreire@gmail.com>
2023-05-10 21:43:21 -0300

    Finalizada a inclusão e listagem de usuários por buffer

--- src/server.js
+++ src/server.js
@@ -1,12 +1,42 @@
 import http from 'http'
 
-const server = http.createServer((req, res)=>{
+const users = []
+
+const server = http.createServer( async (req, res)=>{
   const { method, url } = req
-  console.log(method,url)
  
-  return res.end('hello!!!!');
+  const buffers = []
+  for await (const chunk of req){
+    buffers.push(chunk)
+  }
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (error) {
+    req.body = null  
+  }
+  
+  //list
+  if(method === 'GET' && url === '/users'){
 
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
 
+  }
+
+  //insert
+  if(method === 'POST' && url === '/users'){
+    console.log('post')
+    const { nome, email } = req.body
+    users.push({
+      id:1,
+      nome,
+      email
+    })
+    console.log(users)
+    return res.writeHead(201).end()
+  }
+  return res.writeHead(404).end()
 });
 server.listen(3333);
 
--- stream/stream-http-server.js
+++ stream/stream-http-server.js
@@ -10,9 +10,17 @@ class TransformToNegative extends Transform {
     callback(null, Buffer.from(String(transformed)))
   }
 }
-const server = http.createServer((req, res) => {
-  return req
-    .pipe( new TransformToNegative() )
-    .pipe(res)
+const server = http.createServer( async (req, res) => {
+  const buffers = []
+
+  for await (const chunk of req){
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+  
+  console.log(fullStreamContent)
+
+  return res.end(fullStreamContent)
 })
 server.listen(3334)
